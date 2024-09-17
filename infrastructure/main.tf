3fa8d95b3af7383fcd8d6087c8f8b50d4aa93f57
ehisakhile <akhileehis@gmail.com>
2024-09-17 16:46:56 +0100

    DevOps Challenge

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "master_node" {
   ami           = "ami-0e86e20dae9224db8"
   instance_type = "t2.medium"
-  subnet_id     = aws_subnet.k8s-private-subnet.id
+  subnet_id     = aws_subnet.k8s-private-subnet["az1"].id
   key_name      = aws_key_pair.ec2_key.key_name
   security_groups = [aws_security_group.ec2_sg.name]
 
@@ -16,7 +16,7 @@ resource "aws_instance" "worker_node" {
 
   ami           = "ami-0e86e20dae9224db8"
   instance_type = "t2.medium"
-  subnet_id     = aws_subnet.k8s-private-subnet.id
+  subnet_id     = aws_subnet.k8s-private-subnet["az2"].id
   key_name      = aws_key_pair.ec2_key.key_name
   security_groups = [aws_security_group.ec2_sg.name]
 
