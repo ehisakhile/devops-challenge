4dcfbb2e2cd61637dd1c68d2238ad091d51dc47f
Ehigiator Akhile <akhileehis@gmail.com>
2024-09-19 00:55:24 +0100

    DevOps Challenge

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,24 +1,24 @@
 #Bastion Host - EC2 Instance
 
-resource "aws_instance" "bastion_host" {
-  ami           = "ami-0075013580f6322a1"
-  instance_type = "t2.medium"
-  key_name      = aws_key_pair.ec2_key.key_name
-  subnet_id = aws_subnet.k8s-public-subnet.id
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-  tags = {
-    Name = "${local.environment}-Bastion-Host"
-  }
-}
+# resource "aws_instance" "bastion_host" {
+#   ami           = "ami-05134c8ef96964280"
+#   instance_type = "t2.medium"
+#   key_name      = aws_key_pair.ec2_key.key_name
+#   subnet_id = aws_subnet.k8s-public-subnet.id
+#   vpc_security_group_ids = [aws_security_group.bastion_host_sg.id]
+#   tags = {
+#     Name = "${local.environment}-Bastion-Host"
+#   }
+# }
 
 
 # MasterNode - EC2 instance
 resource "aws_instance" "master_node" {
-  ami           = "ami-0075013580f6322a1"
+  ami           = "ami-05134c8ef96964280"
   instance_type = "t2.medium"
   key_name      = aws_key_pair.ec2_key.key_name
-  subnet_id = aws_subnet.k8s-private-subnet.id
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  subnet_id = aws_subnet.k8s-public-subnet.id
+  vpc_security_group_ids = [aws_security_group.k8s_master_sg.id]
   tags = {
     Name = "${local.environment}-MasterNode"
   }
@@ -26,11 +26,11 @@ resource "aws_instance" "master_node" {
 
 # WorkerNode - EC2 instance
 resource "aws_instance" "worker_node" {
-  ami           = "ami-0075013580f6322a1"
+  ami           = "ami-05134c8ef96964280"
   instance_type = "t2.medium"
   key_name      = aws_key_pair.ec2_key.key_name
-  subnet_id = aws_subnet.k8s-private-subnet.id
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  subnet_id = aws_subnet.k8s-public-subnet.id
+  vpc_security_group_ids = [aws_security_group.k8s_worker_sg.id]
 
   tags = {
     Name = "${local.environment}-WorkerNode"
--- infrastructure/sg.tf
+++ infrastructure/sg.tf
@@ -1,16 +1,42 @@
-# Security Group for EC2 instances
-resource "aws_security_group" "ec2_sg" {
-  name        = "ec2-sg"
-  description = "Security group for EC2 instances"
-  vpc_id      = aws_vpc.k8s-vpc.id
+# # Security Group for Bastion Host
+# resource "aws_security_group" "bastion_host_sg" {
+#   name        = "bastion-host-sg"
+#   description = "Security group for EC2 instances"
+#   vpc_id      = aws_vpc.k8s-vpc.id
 
-  # Allow inbound traffic on port 80 (HTTP)
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Open to the world for HTTP access
-  }
+#   # Allow inbound traffic on port 80 (HTTP)
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]  # Open to the world for HTTP access
+#   }
+
+#   # Allow inbound traffic on port 22 (SSH)
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]  # Open to the world for SSH access
+#   }
+
+#   # Allow all outbound traffic
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   tags = {
+#     Name = "bastion-host-sg"
+#   }
+# }
+
+resource "aws_security_group" "k8s_master_sg" {
+  name        = "k8s-master-sg"
+  description = "Kubernetes master node security group"
+  vpc_id      = aws_vpc.k8s-vpc.id
 
   # Allow inbound traffic on port 22 (SSH)
   ingress {
@@ -20,11 +46,101 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]  # Open to the world for SSH access
   }
 
-  # Allow all outbound traffic
+  # Allow traffic for Kubernetes API server (port 6443)
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  # Allow traffic for etcd server client API (port range 2379-2380)
+  ingress {
+    from_port   = 2379
+    to_port     = 2380
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 10259
+    to_port     = 10259
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    from_port   = 10250
+    to_port     = 10250
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 10257
+    to_port     = 10257
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Egress (outbound) rules (allow all by default)
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "-1"  # Allow all protocols
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "k8s-master-sg"
+  }
+}
+
+resource "aws_security_group" "k8s_worker_sg" {
+  name        = "k8s-worker-sg"
+  description = "Kubernetes worker node security group"
+  vpc_id      = aws_vpc.k8s-vpc.id
+
+  # Allow inbound traffic on port 22 (SSH)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Open to the world for SSH access
+  }
+
+
+  ingress {
+    from_port   = 10250
+    to_port     = 10250
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  ingress {
+    from_port   = 10256
+    to_port     = 10256
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow traffic for Kubelet API, Kube-scheduler, Kube-controller-manager (port range 10248-10260)
+  ingress {
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Egress (outbound) rules (allow all by default)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Allow all protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "k8s-worker-sg"
   }
 }
